f1d1a4c89c45faecd09b475a59961b680f25db89
ngarantBeauvoir <nicolas.garant@beauvoir.ca>
2024-07-31 12:27:25 -0400

    chore:comments

--- AEsaver.jsx
+++ AEsaver.jsx
@@ -1,13 +1,14 @@
-projectName = app.project.file.name; // Nom du projet
-projectPath = app.project.path; // Chemin du projet
-console = app.console; // Console de debug
+/*
+======================================
+Fichier: AEsaver.jsx
+Auteur: ngarant
+Creation: 2024-07-15
+Description: Script de sauvergarde automatique pour After Effects
+======================================
+*/
+projectName = app.project.file.name;    // Nom du projet
+projectPath = app.project.file;         // Chemin du projet
+
 
 $.writeln("The project name is ", projectName)
-// alert("Your OS is " + system.osName + " running version " + system.osVersion);
-// confirm("You are: " + system.userName + " running on " + system.machineName + ".");
-// app.project.saveAs(projectName + ".prproj")
-alert(app.project.file.name);
-
-$.writeln(projectPath)
-
 $.writeln("The project has been saved") 
