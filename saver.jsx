51bb207d866f23dea2e70e9381e8f2c8dee8c8b9
ngarantBeauvoir <nicolas.garant@beauvoir.ca>
2024-07-31 12:07:17 -0400

    feat:projectname

--- saver.jsx
+++ saver.jsx
@@ -3,10 +3,10 @@ projectPath = app.project.path; // Chemin du projet
 console = app.console; // Console de debug
 
 $.writeln("The project name is ", projectName)
-alert("Your OS is " + system.osName + " running version " + system.osVersion);
-confirm("You are: " + system.userName + " running on " + system.machineName + ".");
+// alert("Your OS is " + system.osName + " running version " + system.osVersion);
+// confirm("You are: " + system.userName + " running on " + system.machineName + ".");
 // app.project.saveAs(projectName + ".prproj")
-console.log(app.project)
+alert(app.project.file);
 
 $.writeln(projectPath)
 
