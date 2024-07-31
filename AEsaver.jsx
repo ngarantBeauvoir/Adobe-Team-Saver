fb5e41f8af8e2cadac2d5fcfd1aa9550964b33c7
ngarantBeauvoir <nicolas.garant@beauvoir.ca>
2024-07-31 12:16:48 -0400

    fix:AE name

--- AEsaver.jsx
+++ AEsaver.jsx
@@ -1,4 +1,4 @@
-projectName = app.project.name; // Nom du projet
+projectName = app.project.file.name; // Nom du projet
 projectPath = app.project.path; // Chemin du projet
 console = app.console; // Console de debug
 
@@ -6,7 +6,7 @@ $.writeln("The project name is ", projectName)
 // alert("Your OS is " + system.osName + " running version " + system.osVersion);
 // confirm("You are: " + system.userName + " running on " + system.machineName + ".");
 // app.project.saveAs(projectName + ".prproj")
-alert(app.project.file);
+alert(app.project.file.name);
 
 $.writeln(projectPath)
 
