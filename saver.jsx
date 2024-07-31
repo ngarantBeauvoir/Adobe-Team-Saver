c93fd2e00ae2f6edfa06d09a17edcf17b89ae39b
ngarantBeauvoir <nicolas.garant@beauvoir.ca>
2024-07-31 12:08:05 -0400

    rebase:fix

--- saver.jsx
+++ saver.jsx
@@ -1,10 +1,13 @@
 projectName = app.project.name; // Nom du projet
 projectPath = app.project.path; // Chemin du projet
+console = app.console; // Console de debug
 
 $.writeln("The project name is ", projectName)
-
-app.project.saveAs(projectName + ".prproj")
+alert("Your OS is " + system.osName + " running version " + system.osVersion);
+confirm("You are: " + system.userName + " running on " + system.machineName + ".");
+// app.project.saveAs(projectName + ".prproj")
+console.log(app.project)
 
 $.writeln(projectPath)
 
-$.writeln("The project has been saved")
+$.writeln("The project has been saved") 
